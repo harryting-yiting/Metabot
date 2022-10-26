1dff85fa5a7c92f1873a942fe7351638d70734e2
Hly-123 <55345284+Hly-123@users.noreply.github.com>
2022-10-26 10:05:28 +0800

    Publish two pose and six joints from unity to ROS

---
+++ unity/PickandPlacePublisher.cs
@@ -0,0 +1,86 @@
+using System;
+using RosMessageTypes.Geometry;
+using RosMessageTypes.NiryoMoveit;
+using Unity.Robotics.ROSTCPConnector;
+using Unity.Robotics.ROSTCPConnector.ROSGeometry;
+using Unity.Robotics.UrdfImporter;
+using UnityEngine;
+
+public class PickandPlacePublisher : MonoBehaviour
+{
+    const int k_NumRobotJoints = 6;
+
+    public static readonly string[] LinkNames =
+        { "world/base_link/shoulder_link", "/arm_link", "/elbow_link", "/forearm_link", "/wrist_link", "/hand_link" };
+
+    // Variables required for ROS communication
+    [SerializeField]
+    // [hly] m_TopicName need to change, for example<PickPlaceOrder>
+    string m_TopicName = "/niryo_joints";
+
+    [SerializeField]
+    GameObject m_NiryoOne;
+    [SerializeField]
+    GameObject m_Target;
+    [SerializeField]
+    GameObject m_TargetPlacement;
+    readonly Quaternion m_PickOrientation = Quaternion.Euler(90, 90, 0);
+
+    // Robot Joints
+    UrdfJointRevolute[] m_JointArticulationBodies;
+
+    // ROS Connector
+    ROSConnection m_Ros;
+
+    void Start()
+    {
+        // Get ROS connection static instance
+        m_Ros = ROSConnection.GetOrCreateInstance();
+        // [hly] MsgName need to change, for example<pickAndplaceMsg>
+        // [hly] from RosMessageTypes.NiryoMoveit;
+        m_Ros.RegisterPublisher<NiryoMoveitJointsMsg>(m_TopicName);
+
+        m_JointArticulationBodies = new UrdfJointRevolute[k_NumRobotJoints];
+
+        var linkName = string.Empty;
+
+        // [hly] read robot urdf information
+        for (var i = 0; i < k_NumRobotJoints; i++)
+        {
+            linkName += LinkNames[i];
+            m_JointArticulationBodies[i] = m_NiryoOne.transform.Find(linkName).GetComponent<UrdfJointRevolute>();
+        }
+    }
+
+    public void Publish()
+    {
+        // [hly] define message which need to publish
+        var sourceDestinationMessage = new NiryoMoveitJointsMsg();
+
+        for (var i = 0; i < k_NumRobotJoints; i++)
+        {
+            // [hly] read robot urdf information, each joint position which has been written in .urdf
+            sourceDestinationMessage.joints[i] = m_JointArticulationBodies[i].GetPosition();
+        }
+
+        // [hly]--------------this part is obtain the pick pose and place pose from handi--------------
+        // Pick Pose
+        sourceDestinationMessage.pick_pose = new PoseMsg
+        {
+            position = m_Target.transform.position.To<FLU>(),
+            orientation = Quaternion.Euler(90, m_Target.transform.eulerAngles.y, 0).To<FLU>()
+        };
+
+        // Place Pose
+        sourceDestinationMessage.place_pose = new PoseMsg
+        {
+            position = m_TargetPlacement.transform.position.To<FLU>(),
+            orientation = m_PickOrientation.To<FLU>()
+        };
+        // [hly]--------------this part is obtain the pick pose and place pose from handi--------------
+
+        // [hly] since communicating through tcpConnector, receiver is server_endpoint.py maybe is in ros-tcp-point
+        // Finally send the message to server_endpoint.py running in ROS
+        m_Ros.Publish(m_TopicName, sourceDestinationMessage);
+    }
+}
